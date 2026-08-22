e87d41a34ee0665c0b6029e9c9279cebbe0838ff
Subramanya-2006 <subramanya.s2006@gmail.com>
2026-08-22 12:51:18 +0530

    Passing multiple props

--- day/day 13/day-13-react-app/src/App.jsx
+++ day/day 13/day-13-react-app/src/App.jsx
@@ -1,11 +1,16 @@
 import { Welcome } from "./components/P1";
+import { ProductCard } from "./components/P2";
+import './App.css'
 function App() {
-  
+
   return (
     <>
-     <Welcome name="Subramanya S"/>
+     <Welcome name="Subramanya"/>
+     <ProductCard 
+     name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
+     price={15999}
+     image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg" />
+     
     </>
   )
 }
-
-export default App
